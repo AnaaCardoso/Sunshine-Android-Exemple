a50ac1309c8d6d95bf2afcc241f04d40d5cae59f
AnaaCardoso <ana.lc18@gmail.com>
2016-01-21 18:30:58 -0200

    Sunshine 1.02 ListView com dados fake

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.sunshine.app;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
--- Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,15 +1,25 @@
 package com.example.android.sunshine.app;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ListView lvData;
+
 
     public MainActivityFragment() {
     }
@@ -17,6 +27,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sol - 21/23",
+                "Tomorrow - Sol - 25/30",
+                "Saturday - Sol - 17/23",
+                "Sunday - Sol - 19/27"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray)
+        );
+
+        ArrayAdapter<String> mForeCastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textView,
+                weekForecast);
+
+        lvData = (ListView) v.findViewById(R.id.listview_forecast);
+        lvData.setAdapter(mForeCastAdapter);
+
+        return v;
     }
+
+
 }
